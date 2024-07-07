a87ae49153b880cd0742196fcdfd0b8ca4ab4ecc
Preedee Ponchevin <preedee.digital@gmail.com>
2024-07-07 12:45:36 +0700

    update build docker image

--- build_docker.sh
+++ build_docker.sh
@@ -1,4 +1,4 @@
 #docker rmi -f $(docker images -f “dangling=true” -q)
 
 docker rmi -f prompt_pay_service:latest
-docker build -t prompt_pay_service .
+docker build --platform linux/amd64 -t prompt_pay_service .
